1410926c910ad01b82b100522227127dfb528384
Maria Fernanda Castilho <castilhomariafer13@gmail.com>
2024-09-18 08:19:11 -0300

    Concluido arquivo de conexão com bando de dados

--- models/database.php
+++ models/database.php
@@ -1,3 +1,5 @@
+<!-- Método de conexão com banco de dados utilizando método PDO (mais seguro) -->
+
 <?php
 class Database{
     // Utiliza o padrão Singleton, cujo o objetivo é garantir que apenas uma única instância de uma classe seja criada durante a execução de programa, e que essa instância seja utilizada sempre que necessário.
@@ -10,7 +12,14 @@ class Database{
             $db         = 'sistema_usuarios';
             $user       = 'root';
             $password   = '';
+
+            // A conexão usa o drive Mysql (mysql:) e as informações de host e BD
+            self::$instance = new PDO("msql:host=$host; dbname=$db", $user, $password);
+
+            // Define o modo de erro para execuções, facilitando a depuração e tratamento dos erros
+            self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
+        return self::$instance;
     }
 }
 ?>
