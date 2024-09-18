679c098793dba5a1eb8cd0e2a57afdf989a7e9ad
Maria Fernanda Castilho <castilhomariafer13@gmail.com>
2024-09-18 08:32:36 -0300

    Dentro de user.php finalizado função de busca

--- models/user.php
+++ models/user.php
@@ -2,4 +2,22 @@
 // Inclui o arquivo de conexão que contem a classe Database para gerenciar a conexão com o BD
 require_once 'models/database.php';
 
+class User
+{
+    // Função para encontrar um usuário pelo email
+    public static function findByEmail($email){
+        // Coleta a conexão com o BD
+        $conn = Database::getConnection();
+
+        // Quando utiliza um metodo do PDO utilizamos -> 
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
+
+        // Executa a consulta com e-mail passado como parametro
+        $stmt->execute(['email' => $email]);
+
+        // Retorna os dados do usuário encontrado como um array associativo 
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
+
 ?>
